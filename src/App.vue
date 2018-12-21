e4bb5080d4db9f88ad2d3a95f91ed2d8caf9372e
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2018-12-21 12:06:52 -0500

    demo2

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item['.key']}} - {{item['.value']}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+import {db} from './firebase';
 
 export default {
   name: 'app',
+  firebase: {
+    items:  db.ref('todos')
+  },
   components: {
     HelloWorld
   }
@@ -24,5 +34,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
